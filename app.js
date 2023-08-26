54e76fe65088ae6d9e2430d69e9b0f9babb7dadd
Aditya <ag7023587@gmail.com>
2023-08-27 03:17:41 +0530

    Simple_calculator

---
+++ app.js
@@ -0,0 +1,28 @@
+const buttons= document.querySelectorAll('button')
+const screen= document.getElementById('screen')
+
+for(let button of buttons){
+    button.addEventListener('click',(e)=>{
+        const btnText= e.target.innerText;
+        
+        if(btnText==='C'){
+            screen.value=''
+        }
+
+        else if(btnText==='X'){
+            screen.value+='*'
+        }
+
+        else if(btnText==='='){
+            try{
+                screen.value=eval(screen.value)
+            }
+            catch(e){
+                screen.value= 'Invalid Operation'
+            }
+        }
+        else{
+            screen.value+=btnText
+        }
+    })
+}
